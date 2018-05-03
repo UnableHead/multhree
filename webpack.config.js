9c835356ec7a725b3a885c30fd338976f6cbb4d6
UnableHead <38937004+UnableHead@users.noreply.github.com>
2018-05-03 10:44:31 +0200

    [Basic] Creating Structure  - Added html-webpack-plugin package

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management"
+        })
+    ],
     module: {
         rules: [{
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: ["style-loader", "css-loader"]
         },{
             test: /\.(png|svg|jpg|gif)$/,
-            use: ['file-loader']
+            use: ["file-loader"]
         }]
     }
 };
