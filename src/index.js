9f933bac87aad76aa38541079e442be6d561e0af
UnableHead <38937004+UnableHead@users.noreply.github.com>
2018-05-03 10:57:37 +0200

    [Basic] Creating Structure  - Added Script

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import _ from "lodash";
+import printMe from "./print.js";
 import "./CSS/style.css";
 import Icon from "../Asset/icon.jpg";
 
 function component(){
     const element = document.createElement("div");
+    const btn = document.createElement("button");
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(["Hello", "webpack"], " ");
     element.classList.add("hello");
+
+    btn.innerHTML = "Click me and check the console!";
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     // Add the image to our existing div.
     const myIcon = new Image();
